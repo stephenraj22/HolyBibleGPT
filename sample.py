8d07ba2c53a2bb973bafc05d0610cf4a42727bcc
stephenraj22 <xerananymous33@google.com>
2024-04-19 17:42:10 +0530

    message change for testing

--- sample.py
+++ sample.py
@@ -79,7 +79,10 @@ else:
 if start.startswith('FILE:'):
     with open(start[5:], 'r', encoding='utf-8') as f:
         start = f.read()
+start="How to overcome depression?"
+print(start, "stephen")
 start_ids = encode(start)
+print(start_ids, "stephen")
 x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 
 # run generation
